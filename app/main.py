21bddf96e2c8f8ebf3fcb216fa84e4cc3eaa201d
PlayWithDen4ik <velichkodenis34@gmail.com>
2026-01-31 22:15:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+from os import makedirs, remove
+
+
+def move_file(command: str) -> None:
+    cm, source, dest = command.split()
+    if cm != "mv":
+        return
+    try:
+        with open(source, "r") as file1, open(dest, "w") as file2:
+            file2.write(file1.read())
+            remove(source)
+    except FileNotFoundError:
+        makedirs("/".join(dest.split("/")[:-1]), exist_ok=True)
+        with open(source, "r") as file1, open(dest, "w") as file2:
+            file2.write(file1.read())
+            remove(source)
+    else:
+        return
